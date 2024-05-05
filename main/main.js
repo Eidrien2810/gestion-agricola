f320f27aefb6d17c76b541284c2b6f92293f8cda
angelcruz797 <angelcruzcespedes797@gmail.com>
2024-05-05 19:50:12 -0400

    user menu desplegable
    agrege un menu donde se ve el nombre del usuario en el topbar

--- main/main.js
+++ main/main.js
@@ -297,3 +297,27 @@ function logOut (){
   window.sessionStorage.removeItem("userId")
   window.location.replace("../login/login.html")
 }
+
+//colocar nombre de la usuario
+const userDropbtn = document.getElementById("userDropBtn")
+const user = JSON.parse(window.sessionStorage.getItem("users")).find(obj=>obj.id==userId)
+console.log(user)
+userDropbtn.innerHTML = user.username
+
+function userDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
